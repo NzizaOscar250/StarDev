25460b79a1df865153e683b8aa74eb6ce5598893
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2024-02-26 01:17:06 +0200

    added  Qrcode scann

--- src/view/ScanQrcode.jsx
+++ src/view/ScanQrcode.jsx
@@ -1,45 +1,59 @@
-import  { Component } from 'react'
-import QrReader from 'react-qr-scanner'
 
-class ScanQrcode extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      delay: 100,
-      result: 'No result',
-    }
+import {QrScanner} from '@yudiel/react-qr-scanner';
 
-    this.handleScan = this.handleScan.bind(this)
-  }
-  handleScan(data){
-    this.setState({
-      result: data,
-    })
-  }
-  handleError(err){
-    console.error(err)
-  }
-  render(){
-    const previewStyle = {
-      height: 240,
-      width: 320,
-    }
 
-    return(
-      <div className='min-h-screen flex items-center'>
-         <div className='w-full md:w-1/2 bg-white mx-auto p-2 rounded drop-shadow-sm'>
-        <QrReader
-          delay={this.state.delay}
-          style={previewStyle}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          />
-        <p>{this.state.result}</p>
-      </div>
-      </div>
-    )
-  }
+const ScanQrcode = ()=>{
+
+  return (
+    <QrScanner
+    onDecode={(result) => console.log(result)}
+    onError={(error) => console.log(error?.message)}
+/>
+  )
 }
 
 
-export default ScanQrcode;
+export default ScanQrcode
+
+// class ScanQrcode extends Component {
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       delay: 100,
+//       result: 'No result',
+//     }
+
+//     this.handleScan = this.handleScan.bind(this)
+//   }
+//   handleScan(data){
+//     this.setState({
+//       result: data,
+//     })
+//   }
+//   handleError(err){
+//     console.error(err)
+//   }
+//   render(){
+//     const previewStyle = {
+//       height: 240,
+//       width: 320,
+//     }
+
+//     return(
+//       <div className='min-h-screen flex items-center'>
+//          <div className='w-full md:w-1/2 bg-white mx-auto p-2 rounded drop-shadow-sm'>
+//         <QrReader
+//           delay={this.state.delay}
+//           style={previewStyle}
+//           onError={this.handleError}
+//           onScan={this.handleScan}
+//           />
+//         <p>{this.state.result}</p>
+//       </div>
+//       </div>
+//     )
+//   }
+// }
+
+
+// export default ScanQrcode;
